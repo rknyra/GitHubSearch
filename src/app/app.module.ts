bb12c5f0b4ce5fc4dea8ba5388e6e63a8c274003
RKNyra <rk.nyra@gmail.com>
2019-09-28 04:55:44 +0300

    add the three foundational components (Home, About and Search) that will be used to create the ghSearch webpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './home/about/about.component';
+import { ExploreComponent } from './home/explore/explore.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ExploreComponent
   ],
   imports: [
     BrowserModule,
